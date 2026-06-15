0310473b2a0d40f9e5d5fcf225ea357330dc3ac3
chambers29 <jjaannuu1112@gmail.com>
2026-06-15 04:17:44 +0200

    Refactor: Move wait and roll functions to rps_helper.py

--- rps.py
+++ rps.py
@@ -1,15 +1,4 @@
-import random
-import time
-
-def wait(n : int):
-    time.sleep(n)
-
-
-def roll(roll_list : list):
-    x = 0
-    y = len(roll_list) - 1
-    rollo = roll_list[random.randint(x, y)]
-    return rollo
+from rps_helper import wait, roll
 
 rps = ["rock","paper","scissors"]
 
